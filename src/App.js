1aefd9bb52b1dd1eb023488d1ad0ba6b4a17191a
Santosh Manoharadas <santoshmanoharadas@gmail.com>
2025-06-06 22:20:35 +0530

    app.j changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-
-import {React , useEffect} from 'react';
+import {React, useEffect} from 'react';
 import AOS from 'aos';
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import routes from './pages';
 import Page404 from './pages/404';
 import Header from './components/header/Header';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    });
+  }, []);
 
-    useEffect(() => {
-        AOS.init({
-          duration : 2000
-        }); 
-      }, []);
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag('config', 'G-ZX0MESXKXV', {
+        page_path: location.pathname,
+      });
+    }
+  }, [location]);
 
-    return (
-        <>
+  return (
+    <>
+      <Header />
 
-            <Header />
+      <Routes>
+        {routes.map((data, idx) => (
+          <Route key={idx} path={data.path} element={data.component} exact />
+        ))}
 
-            <Routes>
-
-                {
-                    routes.map((data,idx) => (
-                        <Route key={idx} path={data.path} element={data.component} exact />
-                    ))
-                }
-
-                <Route path='*' element={<Page404 />} />
-            </Routes>
-
-    
-        </>
-    );
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
